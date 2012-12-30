776681042889cfb97121dd0cff89546f09e47748
Brian Genisio <BrianGenisio@Gmail.com>
2012-12-29 21:58:59 -0500

    Record votes

--- scripts/question-viewmodel.js
+++ scripts/question-viewmodel.js
@@ -3,4 +3,16 @@ App.ViewModels.Question = function(questionData) {
 
 	self.content = questionData.content;
 	self.author = questionData.author;
+	self.votes = ko.observableArray(questionData.votes || []);
+
+	self.voteTally = ko.computed( function() {
+		return self.votes().reduce( function(tally, vote) {
+			return tally + vote.value;
+		}, 0 );
+	});
+
+	self.addVote = function(voter,voteType) {
+		var voteValue = voteType === 'up' ? 1 : -1;
+		self.votes.push( {value: voteValue, voter: voter });
+	}
 }
