45662934230c201e4f60705e669234016b908c22
Brian Genisio <BrianGenisio@Gmail.com>
2012-12-29 21:45:37 -0500

    Acting on the login action

--- scripts/overflow-viewmodel.js
+++ scripts/overflow-viewmodel.js
@@ -2,4 +2,9 @@ App.ViewModels.Overflow = function() {
 	var self = this;
 
 	self.userName = ko.observable("");
+	self.isLoggedIn = ko.observable(false);
+
+	self.loginUser = function() {
+		self.isLoggedIn( self.userName != "");
+	}
 }
